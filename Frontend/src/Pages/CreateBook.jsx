277ea13c20ea2905209c0de70c66acdc51c81965
Dev Shah <busycaesar@gmail.com>
2023-11-02 21:30:37 -0400

    Form component seperated! Working on editArticle page now.

--- Frontend/src/Pages/CreateBook.jsx
+++ Frontend/src/Pages/CreateBook.jsx
@@ -4,18 +4,16 @@ import BackButton from "../Components/BackButton";
 import Spinner from "../Components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import BookInformationForm from "../ProjectComponents/bookInformationForm";
 
 export default function CreateBooks() {
-  const [title, setTitle] = useState(""),
-    [author, setAuthor] = useState(""),
-    [publishYear, setPublishYear] = useState(""),
-    [loading, setLoading] = useState(""),
+  const [loading, setLoading] = useState(""),
     navigate = useNavigate(),
-    handleSaveBook = () => {
-      const data = { title, author, publishYear };
+    handleSaveBook = (bookdata) => {
       setLoading(true);
+      console.log(bookdata);
       axios
-        .post("http://localhost:5555/books", data)
+        .post("http://localhost:5555/books", bookdata)
         .then(() => {
           setLoading(false);
           navigate("/");
@@ -26,44 +24,12 @@ export default function CreateBooks() {
           console.log(err);
         });
     };
-
   return (
     <div className="p-4">
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
       {loading ? <Spinner /> : null}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Title</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Author</label>
-          <input
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
-          <input
-            type="number"
-            value={publishYear}
-            onChange={(e) => setPublishYear(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
-          Save
-        </button>
-      </div>
+      <BookInformationForm handleSaveBook={handleSaveBook} buttonName="Save" />
     </div>
   );
 }
