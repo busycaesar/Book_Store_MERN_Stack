081d4290e4aa39b9ba33016421e2808c3fb1a1ed
Dev Shah <busycaesar@gmail.com>
2023-11-07 16:45:08 -0500

    BookModal's Structure Implemented!

--- Frontend/src/Components/Home/BookCard.jsx
+++ Frontend/src/Components/Home/BookCard.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle } from "react-icons/bi";
+import { BiUserCircle, BiShow } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
+import { useState } from "react";
+import BookModal from "./BookModal";
 
 export default function BookCard(props) {
-  const book = props.book;
+  const [showModal, setShowModal] = useState(false),
+    book = props.book;
   return (
     <div className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
       <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
@@ -23,6 +26,13 @@ export default function BookCard(props) {
         <h2 className="my-1">{book?.author}</h2>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow
+          className="text-3-xl text-blue-800 hover:text-black cursor-pointer"
+          onClick={() => {
+            setShowModal(true);
+            console.log("Show Modal", showModal);
+          }}
+        />
         <Link to={`/books/details/${book?._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
         </Link>
@@ -33,6 +43,9 @@ export default function BookCard(props) {
           <MdOutlineDelete className="text-2xl text-red-800 hover:text-black" />
         </Link>
       </div>
+      {showModal && (
+        <BookModal book={book} onClose={() => setShowModal(false)} />
+      )}
     </div>
   );
 }
--- Frontend/src/Components/Home/BookModal.jsx
+++ Frontend/src/Components/Home/BookModal.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/prop-types */
 import { AiOutlineClose } from "react-icons/ai";
-import { PiBookOpenText } from "react-icons/pi";
+import { PiBookOpenTextLight } from "react-icons/pi";
 import { BiUserCircle } from "react-icons/bi";
 
 export default function BookModal(props) {
@@ -8,9 +9,29 @@ export default function BookModal(props) {
   return (
     <div
       className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
-      onClick={onClose }
+      onClick={onClose}
     >
-      BookModal
+      <div
+        onClick={(event) => event.stopPropagation()}
+        className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
+      >
+        <AiOutlineClose
+          className="absolute right-6 top-6 text-3xl text-3xl text-red-600 cursor-pointer"
+          onClick={onClose}
+        />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book?.publishYear}
+        </h2>
+        <h3 className="my-2 text-gray-500">{book?._id}</h3>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book?.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book?.author}</h2>
+        </div>
+      </div>
     </div>
   );
 }
